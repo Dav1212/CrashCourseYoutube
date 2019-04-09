8527fb327a40e8e57fb579a39e7f4f164a845bb8
David Kim <34044661+Dav1212@users.noreply.github.com>
2019-04-09 19:15:36 -0400

    FIrst commit

---
+++ CrashCourseYoutube/CrashCourseYoutube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrashCourseYoutube
+//
+//  Created by david kim on 4/9/19.
+//  Copyright © 2019 david kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
